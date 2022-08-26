c7553cb9aa1dd23c24ca8322ec06afa837e69915
Tallal <108980921+Tallal64@users.noreply.github.com>
2022-08-25 22:33:07 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    'sm': '480px',
+    'md': '768px',
+    'lg': '976px',
+    'xl': '1440px',
+    extend: {},
+  },
+  plugins: [],
+}
